10a5dbc8027d83765d90bc0b2d73d82df35523fd
chenhongrui.chr <chenhongrui.chr@bytedance.com>
2026-02-21 21:58:33 +0800

    feat: add metrics and multi worker

--- play2048/application/dqn_train.py
+++ play2048/application/dqn_train.py
@@ -4,12 +4,15 @@ import sys
 sys.path.append(".")
 
 from gym_2048.envs.game2048_env import Game2048Env
-
+from stable_baselines3.common.monitor import Monitor
 import gymnasium as gym
 
 from stable_baselines3 import DQN
 
 from stable_baselines3.common.callbacks import BaseCallback
+
+from stable_baselines3.common.vec_env import SubprocVecEnv
+from stable_baselines3.common.utils import set_random_seed
 
 class MaxTileCallback(BaseCallback):
     def __init__(self, verbose=0):
@@ -25,14 +28,37 @@ class MaxTileCallback(BaseCallback):
                 # 如果环境 info 里没有 'max_tile'，可能需要从 obs 提取
                 highest = info["highest"]
                 
-                self.logger.record("rollout/highest_block", highest)
+                self.logger.record_mean("rollout/highest_block", highest)
+
+        self.logger.record("rollout/group_size", len(self.locals["dones"]))
         return True
 
-if __name__ == "__main__":
-    env: Game2048Env = gym.make("2048-v0", render_mode="rgb_array", illegal_move_reward=-1.0)
+def make_env_maker(rank, seed=0):
+    """
+    实用工具函数，用于多进程环境创建
+    """
+    def _init():
+        env = gym.make("2048-v0", render_mode="rgb_array", illegal_move_reward=-2.0)
+        env = Monitor(env)
+        env.reset(seed=seed + rank)
+        return env
+    set_random_seed(seed)
+    return _init
 
-    model = DQN("MlpPolicy", env, verbose=1)
-    model.learn(total_timesteps=1000_0000, callback=MaxTileCallback())
+if __name__ == "__main__":
+    worker_count = 8
+
+    envs = []
+    for i in range(worker_count):
+        envs.append(make_env_maker(i))
+
+    the_game_env = SubprocVecEnv(envs)
+    # the_game_env = envs[0]
+    del envs
+
+
+    model = DQN("MlpPolicy", the_game_env, verbose=1, tensorboard_log="./tensorboard/dqn-1")
+    model.learn(total_timesteps=200_0000, callback=MaxTileCallback())
 
     # vec_env = model.get_env()
     # obs = vec_env.reset()
